b52072d218b9ddfae6ad85d23b7c9eb3f49b6615
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-07-26 00:44:44 +0700

    Update list db

--- js/cr_nas.js
+++ js/cr_nas.js
@@ -4,15 +4,39 @@ class CR_Nas{
         cr.onLoad();
         cr.setColor("#88D66C");
         cr.add_btn_top();
+        cr.loadJs("js/cr_nas_db.js","nas_db","onLoad");
     }
+    
+    upload_file(id_db,api_key){
+        var fileInput = $('#fileInput')[0];
+        var file = fileInput.files[0];
+        if(file) {
+          var storageUrl = 'https://firebasestorage.googleapis.com/v0/b/'+id_db+'/o?uploadType=multipart&name=' + encodeURIComponent(file.name);
 
-    add_db(){
-        var data_db_new={
-            "name":"",
-            "db_url":"",
-            "db_web":""
-        };
-        cr_data.edit(data_db_new);
+          var formData = new FormData();
+          formData.append('file', file);
+
+          $.ajax({
+              url: storageUrl,
+              type: 'POST',
+              data: formData,
+              contentType: false,
+              processData: false,
+              headers: {
+                  'Authorization': 'Bearer '+api_key
+              },
+              success: function(response) {
+                  console.log('Upload successful', response);
+                  cr.msg('Upload successful',"Upload File","success");
+              },
+              error: function(jqXHR, textStatus, errorThrown) {
+                  console.error('Upload failed', textStatus, errorThrown);
+                  cr.msg("Upload Faield","Upload File","error");
+              }
+          });
+      }else{
+        cr.msg("Please Select File","None File","info");
+      }
     }
 }
 
---
+++ js/cr_nas_db.js
@@ -0,0 +1,58 @@
+class CR_Nas_DB{
+
+    list_db=[];
+
+    onLoad(){
+        if(localStorage.getItem("list_db")!=null) this.list_db=JSON.parse(localStorage.getItem("list_db"));
+
+        $.each(this.list_db,function(index,db){
+            db["index"]=index;
+            var dbServerItem=$(`
+                <div class="col-6 col-md-3 col-lg-3">
+                    <div role="button" class="card bg-dark border-rounder text-white w-100">
+                    <div class="card-body">
+                        <div class="card-title"><i class="fas fa-server"></i> ${db.name}</div>
+                        <div class="card-text">${db.id}</div>
+                        <button class="btn btn-sm btn-dark btn_edit"><i class="fas fa-edit"></i> Edit</button>
+                    </div>
+                    </div>
+                </div>
+            `);
+
+            $(dbServerItem).find(".btn_edit").click(()=>{
+                cr_data.edit(db);
+                return false;
+            });
+
+            $(dbServerItem).click(()=>{
+                nas.upload_file(db.id,db.api_key);
+            });
+            $("#list_db").append(dbServerItem);
+        });
+    }
+
+    add_db(){
+        var data_db_new={
+            "name":"",
+            "id":"",
+            "api_key":"",
+            "id_sys":"db"+cr.create_id(4)
+        };
+        cr_data.edit(data_db_new,(data)=>{
+            console.log(data);
+            nas.list_db.push(data);
+            localStorage.setItem("list_db",JSON.stringify(nas.list_db));
+            cr.msg("Add databas success!","Add Db","Success");
+        });
+    }
+
+    import_all(){
+
+    }
+
+    export_all(){
+        cr.download(this.list_db,"list_db.json");
+    }
+}
+var nas_db=new CR_Nas_DB();
+nas.db=nas_db;
